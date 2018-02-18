f38fd3869f0baf80125f8513da3b578cd62830f7
Rob55Rod AKA Splitwirez <Splitwirez@gmail.com>
2018-02-18 12:33:57 -0500

    Added a Solution, because we didn't have one for some reason ._.

---
+++ Start9.SkinDesigner/Start9.SkinDesigner/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace Start9.SkinDesigner
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
